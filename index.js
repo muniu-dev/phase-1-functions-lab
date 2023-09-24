355e78ba97f3d2c2147e362450f8b6686a612d27
Michael <muniu.dev@gmail.com>
2023-09-24 16:24:36 +0300

    Completed Assingnment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks (pickUp){
+    if(pickUp>=42){
+        return pickUp - hq;
+    }else{
+        return hq - pickUp;
+    }
+    
+}
+
+function distanceFromHqInFeet(){
+    return distanceFromHqInBlocks(50) * 264;
+    
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start>=destination){
+        return (start - destination)*264;
+    }else{
+        return (destination-start)*264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+
+    if (distanceTravelledInFeet() <= 400){
+        return distanceTravelledInFeet() * 0;
+
+    }else if (distanceTravelledInFeet() > 400 && distanceTravelledInFeet() <= 2000){
+        return distanceTravelledInFeet() * 0.2;
+
+    }else if(distanceTravelledInFeet() > 2000 && distanceTravelledInFeet() <= 2500 ){
+        return 25;
+
+    }else{
+        return "cannot travel that far";
+    }
+}
+
